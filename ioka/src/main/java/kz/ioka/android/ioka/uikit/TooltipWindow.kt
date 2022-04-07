39867838d62ec6a470b04b8628621fd4cb1b8fc9
nmussa <nmussa@alfabank.kz>
2022-04-07 22:48:16 +0600

    feature: fix tooltip position

--- ioka/src/main/java/kz/ioka/android/ioka/uikit/TooltipWindow.kt
+++ ioka/src/main/java/kz/ioka/android/ioka/uikit/TooltipWindow.kt
@@ -7,26 +7,64 @@ import android.os.Looper
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.animation.*
 import android.widget.PopupWindow
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import kz.ioka.android.ioka.R
+import kz.ioka.android.ioka.util.toPx
+
 
 internal class TooltipWindow(ctx: Context) {
 
-    private val tipWindow: PopupWindow?
+    private var tipWindow: PopupWindow = PopupWindow(ctx)
     private val contentView: View
     private val inflater: LayoutInflater
 
+    private val dismissRunnable by lazy {
+        Runnable {
+            if (tipWindow.isShowing) {
+                val fadeOut: Animation = AlphaAnimation(1f, 0f)
+
+                fadeOut.duration = 200
+
+                fadeOut.setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation?) {}
+
+                    override fun onAnimationEnd(animation: Animation?) {
+                        tipWindow.dismiss()
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) {}
+                })
+
+                tipWindow.contentView.startAnimation(fadeOut)
+            }
+        }
+    }
+
+    private val myHandler = Handler(Looper.getMainLooper())
+
     init {
-        tipWindow = PopupWindow(ctx)
+        tipWindow.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                ctx,
+                R.color.ioka_color_static_transparent
+            )
+        )
         inflater = ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         contentView = inflater.inflate(R.layout.view_tooltip, null)
+
+        tipWindow.setOnDismissListener {
+            myHandler.removeCallbacks(dismissRunnable)
+        }
     }
 
     fun showToolTip(anchor: View) {
-        tipWindow!!.height = ConstraintLayout.LayoutParams.WRAP_CONTENT
+        tipWindow.height = ConstraintLayout.LayoutParams.WRAP_CONTENT
         tipWindow.width = ConstraintLayout.LayoutParams.WRAP_CONTENT
         tipWindow.isOutsideTouchable = true
+
         tipWindow.isTouchable = true
         tipWindow.isFocusable = true
         tipWindow.contentView = contentView
@@ -45,17 +83,16 @@ internal class TooltipWindow(ctx: Context) {
         )
         val contentViewWidth: Int = contentView.measuredWidth
 
-        val positionX: Int = anchorRect.centerX() - contentViewWidth / 2
-        val positionY: Int = anchorRect.bottom - anchorRect.height() / 2
+        val arrowX = anchorRect.centerX() + anchorRect.width() / 2
+
+        val positionX: Int = arrowX - contentViewWidth - 4.toPx.toInt()
+        val positionY: Int = anchorRect.top - contentView.measuredHeight + 8.toPx.toInt()
+
         tipWindow.showAtLocation(anchor, Gravity.NO_GRAVITY, positionX, positionY)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (tipWindow.isShowing) {
-                tipWindow.dismiss()
-            }
-        }, 3000)
+        myHandler.postDelayed(dismissRunnable, 3000)
     }
 
     val isTooltipShown: Boolean
-        get() = tipWindow != null && tipWindow.isShowing
+        get() = tipWindow.isShowing
 }
